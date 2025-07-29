f9f2268b6cfa64cec958f1e70719ab35eef9d903
thimappathesecond <hariharakumar08@gmail.com>
2025-07-29 23:55:07 +0530

    added-PORT for render

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+const port = process.env.PORT || 4000; 
+
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server and DB connected on port 3000");
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
     });
   })
   .catch((err) => console.error(err));
